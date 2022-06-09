c8ade971743f9cc7aae79e92617db1aa9ac54415
Dayane Dias <dayane.doc@hotmail.com>
2022-06-09 16:46:46 -0300

    Aulas 3 e 4

--- main.js
+++ main.js
@@ -15,13 +15,60 @@ for (const element of menuItem) {
   })
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function headerBorder() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
+}
+
+function backToTop() {
+  const backToTop = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTop.classList.add('show')
+  } else {
+    backToTop.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  backToTop()
+  headerBorder()
 })
+
+// SWIPER COROUSEL
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewhell: true,
+  keyboard: true
+})
+
+// SCROLL REVEAL
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .text, #home .image,
+  #about .text, #about .image,
+  #services .header, #services .card,
+  #testimonials .header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
